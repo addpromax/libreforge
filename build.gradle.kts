55b9d8096db18e4723ef2c7d41505f083df990e3
Magic_Bili <124496967+addpromax@users.noreply.github.com>
2023-11-26 22:16:01 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ allprojects {
 
     dependencies {
         compileOnly("org.jetbrains:annotations:23.0.0")
-        compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+        compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 
         compileOnly(fileTree("lib") { include("*.jar") })
     }
